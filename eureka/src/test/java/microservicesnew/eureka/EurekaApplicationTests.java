ce71985049d288e72da7cd511d65df3dab4b9305
Maksim Mashkov <mkmax472@gmail.com>
2020-02-24 11:45:50 +0300

    add eureka registrator

---
+++ eureka/src/test/java/microservicesnew/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package microservicesnew.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
